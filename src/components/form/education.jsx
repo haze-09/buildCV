e7db125138b169c9df833f196c74a3c579aaf3de
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-09-21 20:28:19 +0530

    add degree is working

--- src/components/form/education.jsx
+++ src/components/form/education.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function School({ grade }) {
   function nameHelper(name) {
     return name + grade;
@@ -27,12 +29,54 @@ function School({ grade }) {
   );
 }
 
+function UniversityDegree({ index }) {
+  function nameHelper(name) {
+    return name + index;
+  }
+
+  let degreeName = nameHelper("degreeName");
+  let university = nameHelper("university");
+  let location = nameHelper("universityLocation");
+  let percentage = nameHelper("universityPercentage");
+
+  return (
+    <fieldset>
+      <legend>Degree {index + 1}</legend>
+      <label htmlFor={degreeName}>Degree Name:</label>
+      <input type="text" name={degreeName} id={degreeName} required />
+
+      <label htmlFor={university}>University:</label>
+      <input type="text" name={university} id={university} required />
+
+      <label htmlFor={location}>Location:</label>
+      <input type="text" name={location} id={location} required />
+
+      <label htmlFor={percentage}>Percentage:</label>
+      <input type="number" name={percentage} id={percentage} required />
+    </fieldset>
+  );
+}
+
 function Education() {
+  const [degreeCount, setDegreeCount] = useState(1);
+
+  function addDegree() {
+    setDegreeCount(degreeCount + 1);
+  }
+
+  let degreeComponents = [];
+
+  for(let i = 0; i< degreeCount;i++){
+    degreeComponents.push(<UniversityDegree key={i} index={i} />)
+  }
   return (
     <fieldset>
       <legend>Education</legend>
       <School grade={10} />
       <School grade={12} />
+      {degreeComponents}
+      <button type="button" onClick={addDegree}>Add Degree</button>
+
     </fieldset>
   );
 }
